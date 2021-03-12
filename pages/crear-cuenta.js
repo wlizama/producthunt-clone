2bd9316e7904dbc94c2d585f53695e88ebb11a56
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2021-03-12 10:26:50 -0500

    Componente Crear cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,11 +1,92 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
+import { css } from '@emotion/react';
+import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario'
+
+import useValidacion from '../hooks/useValidacion';
+import validarCrearCuenta from '../validacion/validarCrearCuenta';
+
+const STATE_INICIAL = {
+    nombre: '',
+    email: '',
+    password: ''
+}
+  
 
 const CrearCuenta = () => {
+
+    const { valores, errores, handleSubmit, handleChange, handleBlur } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+    const { nombre, email, password } = valores;
+
+    function crearCuenta() {
+        console.log("exec")
+    }
+
     return (
-        <Layout>
-            <h1>Crear cuenta</h1>
-        </Layout>
+        <div>
+            <Layout>
+                <>
+                <h1
+                    css={css`
+                    text-align: center;
+                    margin-top: 5rem;
+                    `}
+                >Crear Cuenta</h1>
+                <Formulario
+                    onSubmit={handleSubmit}
+                    noValidate
+                >
+                    <Campo>
+                        <label htmlFor="nombre">Nombre</label>
+                        <input 
+                            type="text"
+                            id="nombre"
+                            placeholder="Tu Nombre"
+                            name="nombre"
+                            value={nombre}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </Campo>
+                    { errores.nombre && <Error>{errores.nombre}</Error> }
+        
+                    <Campo>
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            placeholder="Tu Email"
+                            name="email"
+                            value={email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </Campo>
+                    { errores.email && <Error>{errores.email}</Error> }
+
+                    <Campo>
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            placeholder="Tu Password"
+                            name="password"
+                            value={password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </Campo>
+                    { errores.password && <Error>{errores.password}</Error> }
+
+                    <InputSubmit 
+                        type="submit"
+                        value="Crear Cuenta"
+                    />
+                </Formulario>
+                </>
+            </Layout>
+        </div>
     )
 }
 
